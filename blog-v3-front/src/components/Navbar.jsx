4716feb8a5eca6c2a0b22c9dc62b031a695f3f87
joshuasam <joshuasam365@gmail.com>
2024-09-07 14:37:02 +0530

    responsiveness

--- blog-v3-front/src/components/Navbar.jsx
+++ blog-v3-front/src/components/Navbar.jsx
@@ -1,42 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/navbar.css';
 import axios from "axios";
-function Navbar({ url, user,setUser }) {
+
+function Navbar({ url, user, setUser }) {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
     function handleLogout() {
         axios.get(url + '/logout', { withCredentials: true })
-        .then(response => {
-            alert("Logged out successfully");
-            setUser(null)
-            window.location.href = '/'; // Redirect to the login page
-        })
-        .catch(error => {
-            console.error("Error during logout", error);
-        });
-}
+            .then(response => {
+                alert("Logged out successfully");
+                setUser(null);
+                window.location.href = '/'; 
+            })
+            .catch(error => {
+                console.error("Error during logout", error);
+            });
+    }
+
+    function toggleNavbar() {
+        setIsNavOpen(!isNavOpen);
+    }
+
     return (
         <div>
             <nav>
                 <p className="headername">Joshua's Public Blog</p>
-                <div className="nav-comp">
+                <div className="toggle-button" onClick={toggleNavbar}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className={`nav-comp ${isNavOpen ? 'active' : ''}`}>
                     <a href="/" className="nav-element">HOME</a>
                     <a href="/about" className="nav-element">ABOUT US</a>
                     <a href="/contact" className="nav-element">CONTACT</a>
                     <a href="/compose" className="nav-element">COMPOSE</a>
                     {user ?
-                        <div>                
-                            <div className="navbar-username">{user.name}
-                                <img src="/prof.png" alt="" className="profile-pic" />
-                                <button className="logout-btn" onClick={handleLogout}>Logout</button>
-                            </div>
-
-                        </div> :
-                        <div>
+                        <div className="navbar-username">
+                            {user.name}
+                            <img src="/prof.png" alt="" className="profile-pic" />
+                            <button className="logout-btn" onClick={handleLogout}>Logout</button>
+                        </div>
+                        :
+                        <div className="login">
                             <a href="/login" className="nav-element">LOGIN</a>
-                            <a href="/register" className="nav-element">REGISTER</a></div>}
+                            <a href="/register" className="nav-element">REGISTER</a>
+                        </div>}
                 </div>
             </nav>
         </div>
-    )
+    );
 }
 
 export default Navbar;
